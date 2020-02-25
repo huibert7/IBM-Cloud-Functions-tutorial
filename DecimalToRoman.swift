c6e687cd50c00d9af2b6913e9544b1b4772069cf
Huibert Aalbers <50550913+huibert7@users.noreply.github.com>
2020-02-25 08:48:33 -0600

    Update DecimalToRoman.swift

--- DecimalToRoman.swift
+++ DecimalToRoman.swift
@@ -13,6 +13,10 @@ struct Input: Codable {
 struct Output: Codable {
     let romanValue: String
 }
+enum RomanNumberError: Error {
+    case illegalValue
+}
+
 func main(param: Input, completion: (Output?, Error?) -> Void) -> Void {
 
     let thousands:[String?] = {[nil,"M","MM","MMM"]}()
@@ -20,26 +24,26 @@ func main(param: Input, completion: (Output?, Error?) -> Void) -> Void {
     let tens:[String?]      = {[nil,"X","XX","XXX","XL","L","LX","LXX","LXXX","XC"]}()
     let units:[String?]     = {[nil,"I","II","III","IV","V","VI","VII","VIII","IX"]}()
 
-    var tempValueString = ""
-    var tempValue = param.decimalNumber
-
-    if number < 1 || number > 3999 {
-        throw RomanNumberError.illegalValue
+    var tempValueString:String = ""
+    
+    if let intNumber:Int = param.decimalNumber {
+       if intNumber > 0 && intNumber < 4000 {
+          var tempValue = intNumber
+          if tempValue > 1000 {
+              tempValueString = thousands[Int(tempValue/1000)]!
+              tempValue = tempValue % 1000
+          }
+          if tempValue > 100 {
+              tempValueString = tempValueString + hundreds[Int(tempValue/100)]!
+              tempValue = tempValue % 100
+          }
+          if tempValue > 10 {
+              tempValueString = tempValueString + tens[Int(tempValue/10)]!
+              tempValue = tempValue % 10
+          }
+          tempValueString = tempValueString + units[Int(tempValue)]!
+       }
     }
-        
-    if tempValue > 1000 {
-        tempValueString = thousands[Int(number/1000)]!
-        tempValue = tempValue % 1000
-    }
-    if tempValue > 100 {
-        tempValueString = tempValueString + hundreds[Int(tempValue/100)]!
-        tempValue = tempValue % 100
-    }
-    if tempValue > 10 {
-        tempValueString = tempValueString + tens[Int(tempValue/10)]!
-        tempValue = tempValue % 10
-    }
-    tempValueString = tempValueString + units[Int(tempValue)]!
 
     let result = Output(romanValue: tempValueString)
     completion(result, nil)
